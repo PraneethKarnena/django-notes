ab076d9c2bf184b70370d300fd3313f17bed317a
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-09 19:57:33 +0530

    added database model for Note and Sharing

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class SharingModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
+    write = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username} - {str(self.read)} - {str(self.write)}'
+
+class NoteModel(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    notes = models.TextField(null=False, blank=False)
+    created = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.owner.username}'
