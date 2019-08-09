15724c5ae8fec77e184309ce5b573767fba2efaa
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-09 19:01:36 +0530

    configured for Home

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(['GET'])
 def home_view(request):
-    pass
+    """
+    View for Home page. Simply render the Home template.
+    """
+
+    return render(request, 'notes/home.html')
