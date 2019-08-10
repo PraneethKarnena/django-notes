95638d695cfdccfad01246b3ab22bcb19873092f
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-10 22:23:29 +0530

    added route for logging out

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home_page'),
     path('signup/', views.signup_view, name='signup_page'),
     path('signin/', views.signin_view, name='signin_page'),
+    path('signout/', LogoutView.as_view(), name='signout_page'),
     path('dashboard/', views.dashboard_view, name='dashboard_page'),
 ]
