43978d60474e1eec11b32cb05d5088e7ad251968
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-10 21:06:53 +0530

    removed useless code

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,21 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-class SignupForm(forms.ModelForm):
-    username = forms.CharField(min_length=10, max_length=15, required=True)
-    first_name = forms.CharField(min_length=1, required=True)
-    last_name = forms.CharField(min_length=2, required=True)
-    password = forms.CharField(min_length=8, required=True, widget=forms.PasswordInput)
-    confirm_password = forms.CharField(min_length=8, required=True, widget=forms.PasswordInput)
+class SignupForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'password']
-
-    def clean(self):
-        cleaned_data = super(SignupForm, self).clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
-
-        if password != confirm_password:
-            raise forms.ValidationError('Passwords do not match!')
+        fields = ['username', 'first_name', 'last_name', 'password1', 'password2']
