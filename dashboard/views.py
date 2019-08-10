2e864ec8240844d68d5b736fd2ec0fb5982a8c92
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-10 18:35:25 +0530

    added Home view

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,8 +4,5 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(['GET'])
 def home_view(request):
-    """
-    View for Home page. Simply render the Home template.
-    """
-
+    #View for Home page. Simply render the Home template.
     return render(request, 'notes/home.html')
