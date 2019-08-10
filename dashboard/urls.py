e95d92cb4bb7731a229be8c9bc03ed3decf6b623
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-10 21:16:00 +0530

    added route for sign in

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home_page'),
-    path('signup/', views.signup_view, name='signup_page')
+    path('signup/', views.signup_view, name='signup_page'),
+    path('signin/', views.signin_view, name='signin_page'),
 ]
