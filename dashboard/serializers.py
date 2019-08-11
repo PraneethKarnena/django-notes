af739251396cca44eb24596ea029e25ec321ba61
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-11 19:16:14 +0530

    created Notes serializer

---
+++ dashboard/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from . import models
+
+
+class AddNotesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.NoteModel
+        fields = ['title', 'notes']
