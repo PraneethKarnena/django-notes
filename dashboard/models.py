4a940b08ba873c9a38085778d14aa3ea38ae59f4
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-11 10:38:37 +0530

    fixed sharing config

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class SharingModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    read = models.BooleanField(default=False)
+    read = models.BooleanField(default=True)
     write = models.BooleanField(default=False)
 
     def __str__(self):
@@ -15,7 +15,8 @@ class NoteModel(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     notes = models.TextField(null=False, blank=False)
     created = models.DateField(auto_now=True)
-    sharing = models.ManyToManyField(SharingModel)
+    shared_users = models.ManyToManyField(SharingModel)
+    shared = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.title} - {self.owner.username}'
