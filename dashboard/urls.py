ced27fd1045f5ab146ff489d3060683b328be185
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-12 18:34:32 +0530

    added routes for different API

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('api/my-notes/', views.my_notes_api, name='my_notes_api'),
     path('api/add-notes/', views.add_notes_api, name='add_notes_api'),
     path('api/edit-notes/<int:id>/', views.edit_notes_api, name='edit_notes_api'),
-    path('api/delete-notes/<int:id>/', views.delete_notes_api, name='delete_notes_api')
+    path('api/delete-notes/<int:id>/', views.delete_notes_api, name='delete_notes_api'),
+    path('api/get-users/', views.get_users_api, name='get_users_api'),
 ]
