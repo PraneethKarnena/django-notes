f74f6cd28c29b32ff3a704905e0a5e8e60dea537
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-12 18:34:15 +0530

    improved code

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -1,5 +1,13 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from . import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = User
+            fields = ['id', 'username', 'first_name', 'last_name']
 
 
 class AddNotesSerializer(serializers.ModelSerializer):
@@ -9,7 +17,16 @@ class AddNotesSerializer(serializers.ModelSerializer):
         fields = ['title', 'notes']
 
 
+class SharingSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = models.SharingModel
+        fields = '__all__'
+
+
 class MyNotesSerializer(serializers.ModelSerializer):
+    sharing = SharingSerializer(many=True)
 
     class Meta:
         model = models.NoteModel
